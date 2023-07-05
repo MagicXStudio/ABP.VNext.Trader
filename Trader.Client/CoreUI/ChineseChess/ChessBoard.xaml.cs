13c11eba95cf26c1db88155f8b287a19b536234c
AngkorW <AngkorW@ANGKORW-DEV>
2023-07-05 15:58:32 +0800

    ChessBoard

--- Trader.Client/CoreUI/ChineseChess/ChessBoard.xaml.cs
+++ Trader.Client/CoreUI/ChineseChess/ChessBoard.xaml.cs
@@ -1,26 +1,144 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Trader.Client.CoreUI.ChineseChess
 {
     /// <summary>
     /// ChessBoard.xaml 的交互逻辑
     /// </summary>
+
     public partial class ChessBoard : UserControl
     {
+
         public ChessBoard()
         {
             InitializeComponent();
         }
+        private bool _isDown;
+        private bool _isDragging;
+        private Canvas _myCanvas;
+        private UIElement _originalElement;
+        private double _originalLeft;
+        private double _originalTop;
+        private CircleAdorner _overlayElement;
+        private Point _startPoint;
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+            _myCanvas = new Canvas();
+
+            var piece = new ChessPiece();
+            piece.Height = piece.Width = 120;
+
+            Canvas.SetTop(piece, 8);
+            Canvas.SetLeft(piece, 8);
+
+            var tb = new TextBox { Text = "PV=计划值、EV=挣值、AC=实际成本" };
+            Canvas.SetTop(tb, 100);
+            Canvas.SetLeft(tb, 100);
+
+            _myCanvas.Children.Add(piece);
+            _myCanvas.Children.Add(tb);
+
+            _myCanvas.PreviewMouseLeftButtonDown += MyCanvas_PreviewMouseLeftButtonDown;
+            _myCanvas.PreviewMouseMove += MyCanvas_PreviewMouseMove;
+            _myCanvas.PreviewMouseLeftButtonUp += MyCanvas_PreviewMouseLeftButtonUp;
+            PreviewKeyDown += window1_PreviewKeyDown;
+
+            myStackPanel.Children.Add(_myCanvas);
+        }
+
+        private void window1_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape && _isDragging)
+            {
+                DragFinished(true);
+            }
+        }
+
+        private void MyCanvas_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (_isDown)
+            {
+                DragFinished(false);
+                e.Handled = true;
+            }
+        }
+
+        private void DragFinished(bool cancelled)
+        {
+            Mouse.Capture(null);
+            if (_isDragging)
+            {
+                AdornerLayer.GetAdornerLayer(_overlayElement.AdornedElement).Remove(_overlayElement);
+
+                if (cancelled == false)
+                {
+                    Canvas.SetTop(_originalElement, _originalTop + _overlayElement.TopOffset);
+                    Canvas.SetLeft(_originalElement, _originalLeft + _overlayElement.LeftOffset);
+                }
+                _overlayElement = null;
+            }
+            _isDragging = false;
+            _isDown = false;
+        }
+
+        private void MyCanvas_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (_isDown)
+            {
+                if ((_isDragging == false) &&
+                    ((Math.Abs(e.GetPosition(_myCanvas).X - _startPoint.X) >
+                      SystemParameters.MinimumHorizontalDragDistance) ||
+                     (Math.Abs(e.GetPosition(_myCanvas).Y - _startPoint.Y) >
+                      SystemParameters.MinimumVerticalDragDistance)))
+                {
+                    DragStarted();
+                }
+                if (_isDragging)
+                {
+                    DragMoved();
+                }
+            }
+        }
+
+        private void DragStarted()
+        {
+            _isDragging = true;
+            _originalLeft = Canvas.GetLeft(_originalElement);
+            _originalTop = Canvas.GetTop(_originalElement);
+
+            _overlayElement = new CircleAdorner(_originalElement);
+            var layer = AdornerLayer.GetAdornerLayer(_originalElement);
+            layer.Add(_overlayElement);
+        }
+
+        private void DragMoved()
+        {
+            var currentPosition = Mouse.GetPosition(_myCanvas);
+
+            _overlayElement.LeftOffset = currentPosition.X - _startPoint.X;
+            _overlayElement.TopOffset = currentPosition.Y - _startPoint.Y;
+        }
+
+        private void MyCanvas_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.Source == _myCanvas)
+            {
+            }
+            else
+            {
+                _isDown = true;
+                _startPoint = e.GetPosition(_myCanvas);
+                _originalElement = e.Source as UIElement;
+                _myCanvas.CaptureMouse();
+                e.Handled = true;
+            }
+        }
     }
+
 }
---
+++ Trader.Client/CoreUI/ChineseChess/CircleAdorner.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Windows.Documents;
+using System.Windows.Media.Animation;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Shapes;
+
+namespace Trader.Client.CoreUI.ChineseChess
+{
+    public class CircleAdorner : Adorner
+    {
+        private readonly Rectangle _child;
+        private double _leftOffset;
+        private double _topOffset;
+        public CircleAdorner(UIElement adornedElement) : base(adornedElement)
+        {
+            VisualBrush brush = new VisualBrush(adornedElement);
+
+            _child = new Rectangle
+            {
+                Width = adornedElement.RenderSize.Width,
+                Height = adornedElement.RenderSize.Height
+            };
+            DoubleAnimation animation = new DoubleAnimation(0.3, 1, new Duration(TimeSpan.FromSeconds(1)))
+            {
+                AutoReverse = true,
+                RepeatBehavior = RepeatBehavior.Forever
+            };
+            brush.BeginAnimation(Brush.OpacityProperty, animation);
+
+            _child.Fill = brush;
+        }
+
+        protected override int VisualChildrenCount => 1;
+
+        public double LeftOffset
+        {
+            get { return _leftOffset; }
+            set
+            {
+                _leftOffset = value;
+                UpdatePosition();
+            }
+        }
+
+        public double TopOffset
+        {
+            get { return _topOffset; }
+            set
+            {
+                _topOffset = value;
+                UpdatePosition();
+            }
+        }
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            var adornedElementRect = new Rect(AdornedElement.DesiredSize);
+            var renderBrush = new SolidColorBrush(Colors.Green) { Opacity = 0.2 };
+            var renderPen = new Pen(new SolidColorBrush(Colors.Navy), 1.5);
+            const double renderRadius = 5.0;
+
+            drawingContext.DrawRectangle(renderBrush, renderPen, adornedElementRect);
+            drawingContext.DrawEllipse(renderBrush, renderPen, adornedElementRect.TopLeft, renderRadius, renderRadius);
+            drawingContext.DrawEllipse(renderBrush, renderPen, adornedElementRect.TopRight, renderRadius, renderRadius);
+            drawingContext.DrawEllipse(renderBrush, renderPen, adornedElementRect.BottomLeft, renderRadius, renderRadius);
+            drawingContext.DrawEllipse(renderBrush, renderPen, adornedElementRect.BottomRight, renderRadius,
+                renderRadius);
+        }
+
+        protected override Size MeasureOverride(Size constraint)
+        {
+            _child.Measure(constraint);
+            return _child.DesiredSize;
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            _child.Arrange(new Rect(finalSize));
+            return finalSize;
+        }
+
+        protected override Visual GetVisualChild(int index) => _child;
+
+        private void UpdatePosition()
+        {
+            var adornerLayer = Parent as AdornerLayer;
+            adornerLayer?.Update(AdornedElement);
+        }
+
+        public override GeneralTransform GetDesiredTransform(GeneralTransform transform)
+        {
+            var result = new GeneralTransformGroup();
+            result.Children.Add(base.GetDesiredTransform(transform));
+            result.Children.Add(new TranslateTransform(_leftOffset, _topOffset));
+            return result;
+        }
+    }
+}
